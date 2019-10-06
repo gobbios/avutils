e7039507db8ebda59d591c317eb6cc0913a79d1a
Christof Neumann <christofneumann1@gmail.com>
2019-10-06 23:55:27 +0200

    summary_elan() now handles multiple files

--- R/summary_elan.R
+++ R/summary_elan.R
@@ -1,41 +1,50 @@
 #' summarize Elan .eaf file
 #'
-#' @param x either character string, providing the path to the elan file, or an \code{xml_document} (read by \code{xml2::read_xml()})
+#' @param x either character string, providing the path to the elan file(s)
 #'
-#' @return a data frame
+#' @return a list with two elements. The first is a data.frame with summaries and the second is a character vector with annotations ordered by their number of occurences (first is the most frequent)
 #' @export
 #'
 
 summary_elan <- function(x) {
-  X <- read_elan(x)
+  res <- matrix(ncol = 6, nrow = 0)
+  annos <- c()
 
-  # file name
-  res <- data.frame(filename = rep(basename(x), nlevels(X$tier)))
+  for (i in 1:length(x)) {
+    X <- read_elan(x[i])
 
-  # tier names
-  res$tier <- levels(X$tier)
+    # file name
+    tempres <- data.frame(filename = rep(basename(x[i]), nlevels(X$tier)),
+                          stringsAsFactors = FALSE)
+    # tier names
+    tempres$tier <- levels(X$tier)
 
-  # number of annotations
-  res$n_anno <- tapply(X$content, X$tier, length)
+    # number of annotations
+    tempres$n_anno <- tapply(X$content, X$tier, length)
 
-  # mean duration of annotations
-  dur <- X$end - X$start
-  res$mean_dur <- round(tapply(dur, X$tier, mean)/1000, 2)
+    # mean duration of annotations
+    dur <- X$end - X$start
+    tempres$mean_dur <- round(tapply(dur, X$tier, mean)/1000, 2)
 
-  # check for empty/NA annotation values
-  foo <- function(xdata) {
-    length(which(xdata == "" | xdata == " " | is.na(xdata)))
+    # check for empty/NA annotation values
+    foo <- function(xdata) {
+      length(which(xdata == "" | xdata == " " | is.na(xdata)))
+    }
+    tempres$empty_anno <- tapply(X$content, X$tier, foo)
+
+    # most frequent anno
+    foo <- function(xdata) {
+      names(sort(table(as.character(xdata)), decreasing = TRUE))[1]
+    }
+    tempres$most_freq_anno <- tapply(X$content, X$tier, foo)
+
+    annos <- c(annos, X$content)
+
+    res <- rbind(res, tempres)
   }
-  res$empty_anno <- tapply(X$content, X$tier, foo)
 
   # count annos
-  annos <- names(sort(table(as.character(X$content)), decreasing = TRUE))
-
-  # most frequent anno
-  foo <- function(xdata) {
-    names(sort(table(as.character(xdata)), decreasing = TRUE))[1]
-  }
-  res$most_freq_anno <- tapply(X$content, X$tier, foo)
+  annos <- names(sort(table(annos), decreasing = TRUE))
 
   list(summary = res, annos = annos)
 }
