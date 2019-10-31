7f684abc495472985b5127b899b819d6d27ba884
Christof Neumann <christofneumann1@gmail.com>
2019-10-31 23:18:54 +0100

    dedicated function to read rttm

---
+++ R/read_rttm.R
@@ -0,0 +1,22 @@
+#' read rttm files
+#'
+#' read and prettify rttm files
+#'
+#' @param x character, path to rttm file
+#' @details The function adds an end time column and assigns meaningful column names for the most relevant columns. The actual content of the eighth column (\code{tier}) depends on the rttm file. For example, 'yunitator' rttm files have levels \code{CHI}, \code{FEM} and \code{MAL}, while SAD outputs contain \code{speech} (or in the case of the full noisemes module other labels like \code{background}, \code{noise} etc).
+#' @return a data.frame
+#' @export
+#'
+#' @examples
+#' rttm <- system.file("noisemesSad_synthetic_speech_overlap.rttm", package = "avutils")
+#' read_rttm(rttm)
+#' rttm <- system.file("noisemesFull_synthetic_speech_overlap.rttm", package = "avutils")
+#' read_rttm(rttm)
+#' rttm <- system.file("yunitator_english_synthetic_speech_overlap.rttm", package = "avutils")
+#' read_rttm(rttm)
+read_rttm <- function(x) {
+  x <- read.table(x, header = FALSE, sep = " ")
+  colnames(x)[c(4, 5, 8)] <- c("start", "dur", "tier")
+  x$end <- x$start + x$dur
+  x
+}
