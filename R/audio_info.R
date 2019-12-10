83a42981cca987d34316260c18262731811b126f
Christof Neumann <christofneumann1@gmail.com>
2019-12-10 10:01:18 +0100

    integrate ffmpeg for audio_info() (fix #9)

--- R/audio_info.R
+++ R/audio_info.R
@@ -1,42 +1,98 @@
 #' get media information
 #'
-#' audio information via sox
+#' audio information via sox or ffmpeg
 #' @param filein character, path to audio files
 #' @param pathtosox character, path to sox binary
-#'
-#' @return a dataframe with features of audio files
+#' @param pathtoffmpeg character, path to ffmepg binary
+#' @param use_sox logical, should \code{sox} be used (default is \code{TRUE}, and if \code{FALSE} \code{ffmpeg} is used)
+#' @details Getting information about audio files is possible both via \code{sox} and \code{ffmpeg}. The advantage of \code{sox} is that it provides more detailed information, but does not by default work with some file types (e.g. mp3). \code{ffmpeg} on the other seems to work with more file types, but returns less detailed information. If you use the default option (i.e. \code{use_sox = TRUE}) and see missing information in the output it might be a good idea to try via \code{ffmpeg}, i.e. \code{use_sox = FALSE}.
+#' @return a \code{data.frame} with features of audio files
 #' @export
 #'
 
-audio_info <- function(filein, pathtosox = getOption("avutils_sox")) {
-  if (!file.exists(pathtosox)) stop("sox binary not found", call. = FALSE)
+audio_info <- function(filein, pathtosox = getOption("avutils_sox"), pathtoffmpeg = getOption("avutils_ffmpeg"), use_sox = TRUE) {
+  if (use_sox) {
+    if (!file.exists(pathtosox)) {
+      stop("sox binary not found", call. = FALSE)
+    }
+    commands <- paste("--i", shQuote(filein))
+    bin <- pathtosox
+  } else {
+    if (!file.exists(pathtoffmpeg)) {
+      stop("ffmpeg binary not found", call. = FALSE)
+    }
+    commands <- paste("-i", shQuote(filein), "-hide_banner")
+    bin <- pathtoffmpeg
+  }
 
-  filein <- normalizePath(filein, winslash = "/")
+  filein <- normalizePath(filein, winslash = "/", mustWork = FALSE)
 
   out <- data.frame(filename = filein, channels = NA, samplerate = NA,
                     resol = NA, samples = NA, duration = NA, format = NA,
                     filesize = NA, filesize_sox = NA)
+
   for (i in 1:nrow(out)) {
-    if (!file.exists(filein[i])) stop("file not found", call. = FALSE)
+    if (!file.exists(filein[i])) {
+      warning(shQuote(filein[i]), " not found", call. = FALSE)
+    } else {
+      cm <- commands[i]
+      res <- suppressWarnings(system2(command = bin,
+                                      args = cm,
+                                      stdout = TRUE,
+                                      stderr = TRUE))
 
-    cm <- paste("--i", shQuote(filein[i]))
-    res <- system2(command = pathtosox,
-                   args = cm,
-                   stdout = TRUE,
-                   stderr = TRUE)
-    res1 <- unlist(strsplit(x = res, split = ":", fixed = TRUE))
-    res1 <- gsub(pattern = " ", replacement = "", x = res1)
-    res2 <- unlist(strsplit(x = res, split = " ", fixed = TRUE))
-    res3 <- unlist(strsplit(x = res, split = ":", fixed = TRUE))
+      if (use_sox) {
+        if (!is.null(attr(res, "status"))) {
+          if (attr(res, "status") == 1) {
+            next
+          }
+        }
+        res1 <- unlist(strsplit(x = res, split = ":", fixed = TRUE))
+        res1 <- gsub(pattern = " ", replacement = "", x = res1)
+        res2 <- unlist(strsplit(x = res, split = " ", fixed = TRUE))
+        res3 <- unlist(strsplit(x = res, split = ":", fixed = TRUE))
 
-    out$channels[i] <- as.numeric(res1[which(res1 == "Channels") + 1])
-    out$samplerate[i] <- as.numeric(res1[which(res1 == "SampleRate") + 1])
-    out$resol[i] <- as.numeric(unlist(strsplit(x = res1[which(res1 == "Precision") + 1], split = "-bit", fixed = TRUE)))
-    out$samples[i] <- as.numeric(res2[which(res2 == "samples") - 1])
-    out$duration[i] <- out$samples[i] / out$samplerate[i]
-    out$format[i] <- res3[which(res3 == "Sample Encoding") + 1]
-    out$filesize[i] <- file.size(filein[i])
-    out$filesize_sox[i] <- res1[which(res1 == "FileSize") + 1]
+        out$channels[i] <- as.numeric(res1[which(res1 == "Channels") + 1])
+        out$samplerate[i] <- as.numeric(res1[which(res1 == "SampleRate") + 1])
+        out$resol[i] <- as.numeric(unlist(strsplit(x = res1[which(res1 == "Precision") + 1], split = "-bit", fixed = TRUE)))
+        out$samples[i] <- as.numeric(res2[which(res2 == "samples") - 1])
+        out$duration[i] <- out$samples[i] / out$samplerate[i]
+        out$format[i] <- res3[which(res3 == "Sample Encoding") + 1]
+        out$filesize[i] <- file.size(filein[i])
+        out$filesize_sox[i] <- res1[which(res1 == "FileSize") + 1]
+
+      }
+
+      if (!use_sox) {
+        res1 <- res[grep(pattern = "  Stream ", x = res)]
+        res1 <- unlist(strsplit(x = res1, split = ", "))
+        out$channels[i] <- as.numeric(res1[3] == "stereo") + 1
+        out$samplerate[i] <- as.numeric(unlist(strsplit(res1[2], " "))[1])
+
+        res2 <- unlist(strsplit(x = res1, split = ", "))[1]
+        res2 <- unlist(strsplit(x = res2, split = "Audio: "))[2]
+        res2 <- unlist(strsplit(x = res2, split = " "))[1]
+        if (res2 == "mp3") {
+          out$format[i] <- "mp3"
+        }
+        if (grepl(pattern = "pcm", x = res2)) {
+          out$format[i] <- "pcm/wav"
+        }
+
+        out$filesize[i] <- file.size(filein[i])
+
+        res3 <- res[grep(pattern = "  Duration: ", x = res)]
+        res3 <- unlist(strsplit(x = res3, split = "Duration: "))
+        res3 <- unlist(strsplit(x = res3, split = ", "))[2]
+        res3 <- as.numeric(unlist(strsplit(res3, ":")))
+        out$duration[i] <- res3[1]*60*60 + res3[2]*60 + res3[3]
+      }
+    }
+
+  }
+
+  if (!use_sox) {
+    out$filesize_sox <- NULL
   }
   out
 }
--- R/convert_audio.R
+++ R/convert_audio.R
@@ -1,12 +1,20 @@
 #' convert audio file format
 #'
-#' @param filein character, path to input audio
+#' convert from mp3 and possibly other audio file formats to .wav
+#'
+#' @param filein character, paths to input audio files
 #' @param outformat a list with specifications of the desired output format, see details
-#' @param overwrite logical, should output files be overwritten if it already exists
+#' @param overwrite logical, should output files be overwritten if they already exist
 #' @param pathtoffmpeg character, path to ffmpeg binary
-#' @param pathout character, path were output is stored, by default the same as the source
-#' @details the \code{outformat=} argument currently provides four options, and by default a mono (\code{mono = TRUE}) .wav files is produced (\code{filetpye = "wav"}) with 44.1kHz resolution (\code{res = 441000}).
-#' @return a vector with the paths to the split files
+#' @param pathout character, path were output is written, by default the same as the source
+#' @details The \code{outformat=} argument currently provides three options, and by default a mono (\code{mono = TRUE}) .wav files is produced (\code{filetpye = "wav"}) with 44.1kHz sampling rate (\code{res = 441000}).
+#'
+#' Files will be potentially renamed, i.e. the file extension of the new output file will change to '.wav'.
+#'
+#' Note that if the input file has the same name and is in the same location as the output (e.g. if you convert x.wav with \code{pathout = NULL}), nothing will happen even if you select \code{overwrite = TRUE} because \code{ffmpeg} won't overwrite its own source.
+#'
+#' @return a \code{data.frame} with diagnostics
+#' @importFrom tools file_path_sans_ext
 #' @export
 #'
 convert_audio <- function(filein,
@@ -17,33 +25,91 @@ convert_audio <- function(filein,
                           pathout = NULL,
                           pathtoffmpeg = getOption("avutils_ffmpeg")) {
 
-  # filein = "~/Downloads/john_oliver_replies_to_jack_warner.mp3"
-  # outformat = list(filetype = "wav", res = 44100, mono = TRUE); pathout = NULL
-  # set_binaries(pathtoffmpeg = "~/Documents/utilities/ffmpeg"); pathtoffmpeg = getOption("avutils_ffmpeg")
-
-  xin <- normalizePath(filein)
-  if (!file.exists(xin)) stop("audio file not found", call. = FALSE)
-  if (!file.exists(pathtoffmpeg)) stop("ffmpeg binary not found", call. = FALSE)
-
-  # get file names and folders
-  temp <- unlist(strsplit(xin, "/", fixed = TRUE))
-  if ("" %in% temp) temp <- temp[temp != ""]
-  infilename <- temp[length(temp)]
-  outfilename <- unlist(strsplit(infilename, ".", fixed = TRUE))
-  outfilename <- outfilename[-length(outfilename)]
-  outfilename <- paste(outfilename, collapse = ".")
-  outfilename <- paste0(outfilename, ".", outformat$filetype)
-
-  outloc <- paste0("/", paste(temp[-length(temp)], collapse = "/"), "/", outfilename)
-  if (!is.null(pathout)) {
-    outloc <- paste0(normalizePath(pathout), "/", outfilename)
+  # some prelims
+  xin <- normalizePath(filein, winslash = "/", mustWork = FALSE)
+  check1 <- !vapply(xin, file.exists, FALSE)
+  if (sum(check1) > 0) {
+    mess <- paste(shQuote(names(check1)[check1]), collapse = "\n")
+    warning("the following audio files were not found:\n", mess, call. = FALSE)
+  }
+  if (!file.exists(pathtoffmpeg)) {
+    stop("ffmpeg binary not found", call. = FALSE)
+  }
+  if (outformat$filetype != "wav") {
+    stop("only 'wav' is supported as output file type for now, not '", outformat$filetype, "'", call. = FALSE)
   }
 
-  # create the command
-  cm <- paste(pathtoffmpeg, ifelse(overwrite, "-y", "-n"), "-i", xin, "-vn", "-ar", outformat$res, "-ac", ifelse(outformat$mono, 1, 2), outloc)
-  # run the command
-  system(command = cm, intern = TRUE)
-  #
+  # get file names and folders
+  infolders <- dirname(xin)
+  basenames <- basename(xin)
 
-  outloc
+  # outnames (replace file extension with 'wav')
+  outnames <- paste0(file_path_sans_ext(basenames), ".wav")
+
+  # and set outlocs
+  if (is.null(pathout)) {
+    outlocs <- file.path(infolders, outnames)
+  } else {
+    pathout <- normalizePath(pathout, winslash = "/")
+    outlocs <- file.path(pathout, outnames)
+  }
+
+  outres <- data.frame(infile = basenames,
+                       outfile = basename(outlocs),
+                       duplicate = duplicated(outlocs),
+                       overwritten = FALSE,
+                       success = FALSE,
+                       outloc = outlocs,
+                       stringsAsFactors = FALSE)
+
+  outres$duplicate[outres$outloc %in% outlocs[duplicated(outlocs)]] <- TRUE
+
+
+  if (sum(outres$duplicate) > 0) {
+    warning("You have files that have the same output name and location.\nThese were ignored.", call. = FALSE)
+  }
+
+  for (i in 1:nrow(outres)) {
+    cm <- paste(ifelse(overwrite, "-y", "-n"),
+                "-i", xin[i], "-vn",  # input file igoring video
+                "-ar", outformat$res, # sampling resolution
+                "-ac", ifelse(outformat$mono, 1, 2), # number of channels
+                outlocs[i])
+    if (!outres$dupli[i]) {
+      # special case: output file is the same as input file
+      # won't do anything because ffmpeg can't overwrite its own source
+      if (xin[i] == outres$outloc[i]) {
+        # do nothing
+      } else {
+        # check whether file already exists
+        outalreadythere <- file.exists(outres$outloc[i])
+        if (outalreadythere) {
+          if (overwrite) {
+            info1 <- file.info(outres$outloc[i])[1, "ctime"]
+            res <- suppressWarnings(system2(command = pathtoffmpeg,
+                                            args = cm,
+                                            stdout = TRUE,
+                                            stderr = TRUE))
+            info2 <- file.info(outres$outloc[i])[1, "ctime"]
+            dtime <- as.numeric(difftime(info2, info1, units = "min"))
+            if (dtime > 0) {
+              outres$success[i] <- TRUE
+            }
+            outres$overwritten[i] <- TRUE
+          }
+
+        } else {
+          res <- suppressWarnings(system2(command = pathtoffmpeg,
+                                          args = cm,
+                                          stdout = TRUE,
+                                          stderr = TRUE))
+          if (file.exists(outres$outloc[i])) {
+            outres$success[i] <- TRUE
+          }
+        }
+      }
+    }
+  }
+
+  outres
 }
